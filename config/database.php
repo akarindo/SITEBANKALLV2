66913dd59198c13d4ebc9388c5fee984fac98bc7
dannykoes <sandal2610@gmail.com>
2026-09-14 10:34:52 +0700

    pdo/mysql to /pdo

--- config/database.php
+++ config/database.php
@@ -57,9 +57,12 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-           'options' => extension_loaded('pdo_mysql') ? array_filter([
-    \Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-]) : [],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                \Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                // 'options' => extension_loaded('pdo_mysql') ? array_filter([
+                //     \PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                // ]) : [],
+            ]) : [],
         ],
 
         'mariadb' => [
@@ -79,7 +82,10 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-]) : [],
+            ]) : [],
+            // 'options' => extension_loaded('pdo_mysql') ? array_filter([
+            //         \PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            //     ]) : [],
         ],
 
         'pgsql' => [
@@ -147,7 +153,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
